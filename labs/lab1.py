8a1cea867cb37041cc34a473a5883444737dab65
inoskeh <124117404+inoskeh@users.noreply.github.com>
2023-09-20 11:03:16 +0500

    Решила 1 задание lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,3 +1,9 @@
 import math
 def task1():
-  pass
+  x = float(input(""))
+  y = float(input(""))
+  z = float(input(""))
+  a = ((math.cos(x)*math.cos(y))/ (math.sin(x)*math.sin(y))) * ((math.log1p(x) * math.log1p(y))/(math.log1p(x) + math.log1p(y)))
+  b = math.log10(1 + x) - z**(1/4)*math.sqrt(x) + y**(1/3)
+  print("Значение перменной a =",a)
+  print("Значнние переменной b =",b)
