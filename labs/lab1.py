3ffcc33a96d4da08abad94b0bb9a4613d97cc33b
inoskeh <unknownlife@gmail.com>
2023-09-26 18:46:40 +0500

    zadanie 2

--- labs/lab1.py
+++ labs/lab1.py
@@ -3,8 +3,13 @@ x = float(input("Введите значение переменной x "))
 y = float(input("Введите значение переменной y "))
 z = float(input("Введите значение переменной z "))
 
-    a = ((cos(x) * cos(y)) / (sin(x) * sin(y))) * (log(x) * log(y)) / (log(x) + log(y))
-    b = log10(x + 1) - z ** (1 / 4) * x ** (1 / 2) + y ** (1 / 3)
+a = ((cos(x) * cos(y)) / (sin(x) * sin(y))) * (log(x) * log(y)) / (log(x) + log(y))
+b = log10(x + 1) - z ** (1 / 4) * x ** (1 / 2) + y ** (1 / 3)
+print("Значение переменной a =", a)
+print("Значение переменной b =", b)
 
-    print("{0:.4f} text {1:.4f}".format(a, b))
-    print("Значение переменной b =", b)
+#task2
+a = 2
+b = -1
+f = a*(((x**b) + x)/3) - (x**(b/4))
+print("Функция f(x) = ", f)
