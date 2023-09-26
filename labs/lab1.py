1a86234a4b41a909051efb32524b71bcfc3f2b05
inoskeh <unknownlife@gmail.com>
2023-09-26 17:53:51 +0500

    изменила формулу для нахождения a

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,9 +1,9 @@
 from math import *
-def task1():
-  x = float(input("Введите значение переменной x"))
-  y = float(input("Введите значение переменной y"))
-  z = float(input("Введите значение переменной z"))
-  a = ((cos(x)*cos(y))/ (sin(x)*sin(y))) * ((log1p(x) * log1p(y))/(log1p(x) + log1p(y)))
-  b = log10(1 + x) - z**(1/4)*sqrt(x) + y**(1/3)
-  print("Значение перменной a =",a)
-  print("Значнние переменной b =",b)
+x = float(input("Введите значение переменной x "))
+y = float(input("Введите значение переменной y "))
+z = float(input("Введите значение переменной z "))
+a = ((cos(x)*cos(y))/(sin(x)*sin(y)))*(log(x)*log(y))/(log(x)+log(y))
+b = log10(1 + x) - z**(1/4)*sqrt(x) + y**(1/3)
+print("Значение перменной a =",a)
+print("Значение переменной b =",b)
+
