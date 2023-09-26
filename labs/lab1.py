40b19ad80fc7bb29970f7124c0926c6e6ea3be09
inoskeh <unknownlife@gmail.com>
2023-09-26 18:33:04 +0500

    изменила формулу для нахождения a и b

--- labs/lab1.py
+++ labs/lab1.py
@@ -2,8 +2,9 @@ from math import *
 x = float(input("Введите значение переменной x "))
 y = float(input("Введите значение переменной y "))
 z = float(input("Введите значение переменной z "))
-a = ((cos(x)*cos(y))/(sin(x)*sin(y)))*(log(x)*log(y))/(log(x)+log(y))
-b = log10(x+1) - z**(1/4)*x**(1/2) + y**(1/3)
-print("Значение перменной a =",a)
-print("Значение переменной b =",b)
 
+    a = ((cos(x) * cos(y)) / (sin(x) * sin(y))) * (log(x) * log(y)) / (log(x) + log(y))
+    b = log10(x + 1) - z ** (1 / 4) * x ** (1 / 2) + y ** (1 / 3)
+
+    print("{0:.4f} text {1:.4f}".format(a, b))
+    print("Значение переменной b =", b)
